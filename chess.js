f630e18e39f556ce5c03fce0e8a18fa70f3eca53
Tus1202 <poxpoxx123@gmail.com>
2026-07-30 07:05:44 +0000

    add pieces

--- chess.js
+++ chess.js
@@ -10,3 +10,19 @@ const chessBoard = [
     ["P","P","P","P","P","P","P","P"],
     ["R","N","B","Q","K","B","N","R"]
 ];
+
+const pieces = {
+    K: "images/pieces/whiteKing.png",
+    Q: "images/pieces/whiteQueen.png",
+    R: "images/pieces/whiteRook.png",
+    B: "images/pieces/whiteBishop.png",
+    N: "images/pieces/whiteKnight.webp",
+    P: "images/pieces/whitePawn.png",
+
+    k: "images/pieces/blackKing.png",
+    q: "images/pieces/blackQueen.png",
+    r: "images/pieces/blackRook.png",
+    b: "images/pieces/blackBishop.png",
+    n: "images/pieces/blackKnight.png",
+    p: "images/pieces/blackPawn.png"
+};
